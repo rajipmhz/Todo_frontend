fa083ac06520f93d06d0c3ff92bd4e24d2d3c882
rajip <rajipmhz450@gmail.com>
2026-01-29 17:44:08 +0545

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+    preview: {
+    host: true,
+    allowedHosts: ["todo-frontend-79r1.onrender.com"],
+  },
 })
