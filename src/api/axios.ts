f1a64d604e9aafb2038aad430f6878e645f1309a
rajip <rajipmhz450@gmail.com>
2026-01-29 17:06:26 +0545

    change url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,7 @@ import { refreshApi } from "./auth.api";
 import { clearAuth, getAuthToken, setAuthToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "https://todo-backend-ge6y.onrender.com",
+  baseURL: "https://todo-backend-k6oa.onrender.com",
   withCredentials: true,
 });
 
